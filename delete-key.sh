359fd058b3b44380512df0447f257063df0402d7
marcos <nyrocha018@gmail.com>
2024-01-03 20:05:15 -0300

    chore: improve workflow

--- delete-key.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-uid=$(gpg --list-secret-keys --with-colons | awk -F: '$1 == "sec" {print $5}')
-gpg --delete-secret-key $id
-gpg --delete-key $id
--- gen-key.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gpg --batch --gen-key gen-key-script
-uid=$(gpg --list-secret-keys --with-colons | awk -F: '$1 == "sec" {print $5}')
-gpg --keyserver keyserver.ubuntu.com --send-keys $uid
-echo "waiting..."
-sleep 90
-echo "waiting finish!"
-gpg --keyserver keyserver.ubuntu.com --recv-keys $uid
